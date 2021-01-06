065246f54c43bc426eed4b72caa9298c60d93a56
Juanma Tola <tolajuanmanuel@yahoo.com.ar>
2021-01-06 01:56:33 -0300

    instalo bootstrap y primera estructura del proyecto

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Control de Cobros</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="d-flex justify-content-around py-5">
+        <a href="./modules/clients/create.php" class="btn btn-primary" >Nuevo cliente</a>
+        <a href="./modules/clients/edit.php" class="btn btn-primary" >Editar cliente</a>
+        <a href="./modules/payment/insert.php" class="btn btn-success" >+ Nuevo pago</a>
+    </div>
+
+    <div>
+        ACA VOY A LISTAR CLIENTES E INFORMACION DE DEUDAS
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
