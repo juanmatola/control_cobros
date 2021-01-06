91e05982afdf4a675ceed0ac8ef0188844b0070e
Juanma Tola <tolajuanmanuel@yahoo.com.ar>
2021-01-06 04:06:28 -0300

    Creo directorios y estructura gral de la app

---
+++ modules/clients/create.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Control de Cobros</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <style>
+        body{
+            padding: 0px 50px;
+
+            background-color: rgb(240,240,240);
+            min-height: 100vh;
+        }   
+        .logo{
+            height: 100px;
+            width: auto;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="d-flex justify-content-around align-items-center py-4">
+        <h3>Crear Nuevo Cliente</h3>
+        <img src="/assets/img/logo.png" class="img-fluid logo">
+    </div>
+
+
+     <!-- Bootstrap JS -->
+     <script src="/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
