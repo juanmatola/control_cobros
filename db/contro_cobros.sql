93067ba2cc8b5c6bf3b4e5a3be163c556a0a7973
Juanma Tola <tolajuanmanuel@yahoo.com.ar>
2021-01-08 02:19:03 -0300

    Rediseño de bases de datos

--- db/contro_cobros.sql
+++ db/contro_cobros.sql
@@ -1,18 +1,29 @@
+CREATE TABLE abonos (
+    id          INT AUTO_INCREMENT,
+    nombre      varchar(50) NOT NULL,
+    monto       INT NOT NULL,
+    CONSTRAINT pk_abonos PRIMARY KEY(id)
+);
+
 CREATE TABLE clientes (
     id          INT AUTO_INCREMENT,
+    abono_id    INT,
     nombre      VARCHAR(50) NOT NULL,
     telefono    INT NOT NULL,
     email       VARCHAR(50),
     web         VARCHAR(50),
-    abono       CHAR(2) NOT NULL,
-    CONSTRAINT pk_usuarios PRIMARY KEY(id) 
+    fecha       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_clientes PRIMARY KEY(id),
+    CONSTRAINT fk_cliente_abono FOREIGN KEY(abono_id) REFERENCES abonos(id) 
 );
 
 CREATE TABLE cobros (
     id          INT AUTO_INCREMENT,
     cliente_id  INT NOT NULL,
-    monto    INT NOT NULL,
-    fecha DATE NOT NULL,
+    abono_id    INT,
+    monto       INT NOT NULL,
+    fecha       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT pk_cobros PRIMARY KEY(id),
-    CONSTRAINT fk_cobro_cliente FOREIGN KEY(cliente_id) REFERENCES clientes(id)
+    CONSTRAINT fk_cobro_cliente FOREIGN KEY(cliente_id) REFERENCES clientes(id),
+    CONSTRAINT fk_cobro_abono FOREIGN KEY(abono_id) REFERENCES abonos(id)
 );
